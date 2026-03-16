a04f7b19e8d09442c51f1b7004153e51022ce609
OvAhmet <ahmetovacik523@gmail.com>
2026-03-16 20:22:25 +0300

    Fix: fixing the false wiring and some syntax errors. Also delegated the flag generation to dispatcher (alu now returns carry outputs aswell)

--- src/alu/alu.v
+++ src/alu/alu.v
@@ -4,35 +4,33 @@ module alu(
     input [2:0] parsed_opcode,
     input [7:0] in_B, in_C,
     output [7:0] out_A,
-    output flag_zero, flag_carry, flag_negative, flag_overflow
+    output carry_add, carry_sub
 );
     // Constants
-    wire wire_zero = 1b'0;
-    wire wire_one  = 1b'1;
+    wire wire_zero = 1'b0;
+    wire wire_one  = 1'b1;
     // Module result carriers
-    wire [7:0] out_nop, out_hlt, out_add, out_sub,
-    wire [7:0] out_nor, out_and, out_xor, out_rsh,
-    wire [7:0] out_mux
-    wire carry_add, carry_sub;
+    wire [7:0] out_nop, out_hlt, out_add, out_sub;
+    wire [7:0] out_nor, out_and, out_xor, out_rsh;
  
-    op_nop (out_A);
-    op_hlt (out_A);
-    op_nor (in_B, in_C, out_A); //FLAG
-    op_and (in_B, in_C, out_A); //FLAG
-    op_xor (in_B, in_C, out_A); //FLAG
-    op_rsh (in_C, out_A);
+    op_nop ins_nop (out_nop);
+    op_hlt ins_hlt (out_hlt);
+    op_nor ins_nor (in_B, in_C, out_nor); //FLAG
+    op_and ins_and (in_B, in_C, out_and); //FLAG
+    op_xor ins_xor (in_B, in_C, out_xor); //FLAG
+    op_rsh ins_rsh (in_C, out_rsh);
 
     add_sub_8bit op_add (
         .operand_a(in_B),
         .operand_b(in_C),
-        .is_sub(1b'0),
+        .is_sub(1'b0),
         .result(out_add),
         .carry_out(carry_add)
     ); //FLAG
     add_sub_8bit op_sub (
         .operand_a(in_B),
         .operand_b(in_C),
-        .is_sub(1b'1),
+        .is_sub(1'b1),
         .result(out_sub),
         .carry_out(carry_sub)
     ); //FLAG
@@ -47,6 +45,6 @@ module alu(
         .in_and(out_and), 
         .in_xor(out_xor), 
         .in_rsh(out_rsh), // 8bits outputs
-	    .Z(out_mux)
+	    .Z(out_A)
     );
 endmodule
